cf6727f5d93715ee69fd85dce5f6b77acfd58ed9
LucaGiovannini02 <luca8giovannini@gmail.com>
2024-05-23 20:00:35 +0200

    piccolo fix al drawer

--- src/gitmess-ui/src/lib/components/DeleteRepo.svelte
+++ src/gitmess-ui/src/lib/components/DeleteRepo.svelte
@@ -17,7 +17,7 @@
 <button on:click={myClick}>
     <img src="/trash.png" alt="trash_button" class="w-10" />
 
-    <Drawer width="h-full" position="bottom" >
+    <Drawer width="h-full px-12" position="bottom" >
         <div class="w-full h-full flex justify-center items-center text-black">
             <div class="flex flex-col items-center">
                 Are you sure do you want delete "{repoName}"?
